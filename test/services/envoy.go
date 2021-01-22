ec92d978d7d2f3fc5629c6b60e610b3f270d5991
ashleywang1 <ashley.wang@solo.io>
2021-01-22 12:24:48 -0500

    Hardcode rest_xds_cluster and port for Consul+Vault test

--- test/services/envoy.go
+++ test/services/envoy.go
@@ -111,6 +111,21 @@ static_resources:
                     port_value: {{.Port}}
     http2_protocol_options: {}
     type: STATIC
+  - name: rest_xds_cluster
+    connect_timeout: 5.000s
+    load_assignment:
+      cluster_name: rest_xds_cluster
+      endpoints:
+        - lb_endpoints:
+            - endpoint:
+                address:
+                  socket_address:
+                    address: {{.GlooAddr}}
+                    port_value: 9976
+    upstream_connection_options:
+      tcp_keepalive: {}
+    type: STRICT_DNS
+    respect_dns_ttl: true
 {{if .RatelimitAddr}}
   - name: ratelimit_cluster
     connect_timeout: 5.000s
