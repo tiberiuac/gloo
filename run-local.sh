99f566d917864672e6f293bbffb515a08c2067ea
Mitch Aman <mitch.aman@solo.io>
2021-07-14 10:20:28 -0400

    Remove helper script

--- run-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kind create cluster
-make push-kind-images
-make build-test-chart
-make glooctl-linux-amd64
-_output/glooctl-linux-amd64 install gateway --file _test/gloo-"$(git describe --tags --dirty | cut -c 2-)".tgz
